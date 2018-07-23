e0e691d9143d7a6c32c380bce1a0410c7cf2782f
ahmedkorim <ahmedkorrim@gmail.com>
2018-07-23 00:38:58 -0700

    add header and nav bar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,9 @@
 //var
 const $header = $('.carousel-item');
 const $culturalSecion = $(".cultural .row");
-
+const $sidenav = $('.sidenav');
+const $aside = $("aside");
+const $mainHeader = $('.main-header');
 const headerDim = () => {
     $header.height(window.innerHeight)
 };
@@ -29,3 +31,34 @@ const mapAdjustion = () => {
 
 }
 mapAdjustion();
+const switchSize = () => {
+    if (window.innerWidth > 800) {
+        // disk top
+        if (!$mainHeader.hasClass('hidden')) {
+            $mainHeader.addClass('hidden');
+            $sidenav.removeClass("nano");
+            $(".brand-holder").append($(".brand-heading"));
+        }
+
+    } else {
+        $mainHeader.removeClass("hidden");
+        $sidenav.addClass("nano");
+        $mainHeader.find(".brand").append($('.brand-heading'));
+
+    }
+};
+switchSize();
+$(window).on('resize', switchSize);
+const toggleSidNav = (e) => {
+    e.preventDefault();
+    if ($aside.hasClass("active")) {
+        $aside.removeClass("active");
+        return;
+    }
+    $aside.addClass("active");
+
+
+};
+
+
+$('.nav-toggle').on('click', toggleSidNav)
